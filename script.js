0114a5985a6e7ffebe4763af8a9418a8166a2938
yishak abrham <yishakabrham621@gmail.com>
2022-11-11 23:14:04 +0300

    first draft

---
+++ script.js
@@ -0,0 +1,63 @@
+//declaration
+const companies = document.querySelector('.companies');
+const templates = document.querySelector('.templates');
+const queries = document.querySelector('.queries');
+
+const updateCount = (el) => {
+  const value = parseInt(el.dataset.num);
+  const increment = Math.ceil(value / 1000);
+
+  let initialValue = 0;
+  //count function
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+    //to stop counting when it > value
+    if (initialValue > value) {
+      el.textContent = `10k+`;
+      clearInterval(increaseCount);
+      return; //<!--TODO: important
+    }
+    el.textContent = `${initialValue}K+`;
+  }, 20); //1 mili second
+  //console.log(increaseCount); the id that each span get
+};
+const updateCountTwo = (el) => {
+  const value = parseInt(el.dataset.num);
+  const increment = Math.ceil(value / 1000);
+
+  let initialValue = 0;
+  //count function
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+    //to stop counting when it > value
+    if (initialValue > value) {
+      el.textContent = `${value}`;
+      clearInterval(increaseCount);
+      return; //<!--TODO: important
+    }
+    el.textContent = `${initialValue}`;
+  }, 10); //1 mili second
+  //console.log(increaseCount); the id that each span get
+};
+const updateCountThree = (el) => {
+  const value = parseInt(el.dataset.num);
+  const increment = Math.ceil(value / 1000);
+
+  let initialValue = 0;
+  //count function
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+    //to stop counting when it > value
+    if (initialValue > value) {
+      el.textContent = `12M+`;
+      clearInterval(increaseCount);
+      return; //<!--TODO: important
+    }
+    el.textContent = `${initialValue}M+`;
+  }, 20); //1 mili second
+  //console.log(increaseCount); the id that each span get
+};
+
+updateCount(companies);
+updateCountTwo(templates);
+updateCountThree(queries);
